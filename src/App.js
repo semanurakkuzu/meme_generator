246674ff2c969f743da7e1f2d0a9a6b11b410196
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-12 15:30:28 +0300

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 function App() {
   return (
-<></>
+    <>
+      <button class="outline outline-offset-2 outline-blue-500 ...">Button A</button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
   );
 }
 
