fa88255398ff290593464ae34f134ead6137962f
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-13 13:28:45 +0300

    added memes data

---
+++ src/MemesData.js
@@ -0,0 +1,39 @@
+export default {
+    "success": true,
+    "data": {
+        memes:[
+            {
+            "id": "1",
+            "name": "Two Buttons",
+            "url": "https://imgflip.com/s/meme/Two-Buttons.jpg",
+            "width": "600",
+            "height":"900",
+            "box-count": 1
+        },
+        {
+            "id": "2",
+            "name": "Drake Hotline Blings",
+            "url": "https://a.pinatafarm.com/1200x1200/24b83e1e0a/drake-hotline-bling.jpg",
+            "width": "1200",
+            "height":"1200",
+            "box-count": 2
+        },
+        {
+            "id": "3",
+            "name": "Take My Money",
+            "url": "https://www.meme-arsenal.com/memes/ed3a49701d7dce8d9d1cb5f74a7f79f8.jpg",
+            "width": "1200",
+            "height":"1200",
+            "box-count": 3
+        },
+        {
+            "id": "4",
+            "name": "Cat",
+            "url": "https://i.kym-cdn.com/entries/icons/original/000/041/399/cover1.jpg",
+            "width": "1200",
+            "height":"1200",
+            "box-count": 4
+        }
+    ] 
+    }
+}
