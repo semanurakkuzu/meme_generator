eb535ee56e5abd665c9530569dfb88dc74ec4b71
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-13 13:11:33 +0300

    edited container-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1024px", //1280
+        "2xl": "1024px" //1600
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
