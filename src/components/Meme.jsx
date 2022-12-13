95f8d0b94ee98cac30e97db8998153415bf6e27f
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-13 17:02:39 +0300

    added toptext and bottomtext

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,41 +2,54 @@ import React from "react";
 import memesData from "../MemesData.js";
 
 export default function Meme() {
-
   const [meme, setMeme] = React.useState({
-    topText:"",
-    bottomText:"",
-    randomImage:"https://i.kym-cdn.com/entries/icons/original/000/041/399/cover1.jpg"
+    topText: "",
+    bottomText: "",
+    randomImage:
+      "https://i.kym-cdn.com/entries/icons/original/000/041/399/cover1.jpg",
   });
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData);
-  
+
   function getMemeImage() {
-    console.log("deneme")
     const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url=memesArray[randomNumber].url;
-    setMeme(prevMeme =>({
-        ...prevMeme,
-        randomImage:url
-    }))
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    }))
+  }
+  
   return (
-    <div className="bg-slate-300 pt-8">
+    <div className="bg-slate-200 pt-8">
       <div className="flex flex-row mb-5">
         <div className="w-2/4 flex justify-center">
           <input
             type="text"
+            name="topText"
             className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-violet-500 focus:ring-violet-500 block w-9/12 rounded-md sm:text-sm focus:ring-1"
-            placeholder=""
+            placeholder="Top text"
+            value={meme.topText}
+            onChange={handleChange}
           />
         </div>
         <div className="w-2/4 flex justify-center">
           <input
             type="text"
+            name="bottomText"
             className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-fuchsia-500 focus:ring-fuchsia-500 block w-9/12 rounded-md sm:text-sm focus:ring-1"
-            placeholder=""
+            placeholder="Bottom text"
+            value={meme.bottomText}
+            onChange={handleChange}
           />
         </div>
       </div>
@@ -48,10 +61,15 @@ export default function Meme() {
           Get a new meme image
         </button>
       </div>
-      <div>
-      <img src={meme.randomImage}  className="memeImage"/>
+      <div className="flex justify-center memeImages" style={{backgroundImage: `url(${meme.randomImage})`,}} >
+        <div className="top-text bg-white ">
+          <p className="text-6xl text-black text-center">{meme.topText} </p>
+        </div>
+        <div className="bottom-text bg-white ">
+          <p className="text-6xl text-black text-center">{meme.bottomText} </p>
+          
+        </div>
       </div>
-      
     </div>
   );
 }
