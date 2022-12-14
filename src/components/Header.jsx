90547a8732769efddcd2a4a49f3ac60e09f459aa
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-14 16:38:39 +0300

    edited header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
                 <div className="basis-1/8 text-white ml-3 my-1" >
                     <img src={logo} alt="" className="logo mr-3" />
                 </div>
-                <div className="basis-1/6 text-white mt-2">
+                <div className="basis-1/4 text-white mt-3">
                 <span className="text-2xl">Meme Generator</span>
                 </div>
-                <div className="basis-4/5 text-white text-right mr-3 mt-2">
+                <div className="basis-4/5 text-white text-right mr-3 mt-3">
                     <span className="text-lg">React and Tailwind</span>
                 </div>
             </div>
